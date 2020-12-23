95476852309d97c545dcb71380e3a24957d67c16
acotrina <acotrina@espol.edu.ec>
2020-12-23 10:05:46 -0500

    Data Class / Encapsulate Field - Arlette Cotrina (Correccion)

--- src/modelos/InformacionAdicionalProfesor.java
+++ src/modelos/InformacionAdicionalProfesor.java
@@ -1,36 +1,29 @@
 package modelos;
 
 public class InformacionAdicionalProfesor {
-
     private int añosdeTrabajo;
     private String facultad;
     private double BonoFijo;
-
+    
     public int getAñosdeTrabajo() {
         return añosdeTrabajo;
     }
-
     public String getFacultad() {
         return facultad;
     }
-
     public double getBonoFijo() {
         return BonoFijo;
     }
-
     public void setAñosdeTrabajo(int añosdeTrabajo) {
         this.añosdeTrabajo = añosdeTrabajo;
     }
-
     public void setFacultad(String facultad) {
         this.facultad = facultad;
     }
-
     public void setBonoFijo(double BonoFijo) {
         this.BonoFijo = BonoFijo;
     }
-
-    public double calcularSueldo(Profesor prof) {
+    public double calcularSueldo() {
         double sueldo = 0;
         sueldo = añosdeTrabajo * 600 + BonoFijo;
         return sueldo;
