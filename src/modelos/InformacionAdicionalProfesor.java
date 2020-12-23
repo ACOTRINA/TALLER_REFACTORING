9912089c0c9a5f9c68517713e7d6d9775e28931d
acotrina <acotrina@espol.edu.ec>
2020-12-22 19:08:42 -0500

    Data Class / Encapsulate Field - Arlette Cotrina

--- src/modelos/InformacionAdicionalProfesor.java
+++ src/modelos/InformacionAdicionalProfesor.java
@@ -4,5 +4,31 @@ public class InformacionAdicionalProfesor {
     public int añosdeTrabajo;
     public String facultad;
     public double BonoFijo;
+
+    public int getAñosdeTrabajo() {
+        return añosdeTrabajo;
+    }
+
+    public String getFacultad() {
+        return facultad;
+    }
+
+    public double getBonoFijo() {
+        return BonoFijo;
+    }
+
+    public void setAñosdeTrabajo(int añosdeTrabajo) {
+        this.añosdeTrabajo = añosdeTrabajo;
+    }
+
+    public void setFacultad(String facultad) {
+        this.facultad = facultad;
+    }
+
+    public void setBonoFijo(double BonoFijo) {
+        this.BonoFijo = BonoFijo;
+    }
+    
+    
     
 }
